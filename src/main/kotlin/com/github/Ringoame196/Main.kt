4a0f88a16791b40e44254af45242e52cf80a6276
りんご飴196 <kazumac41@gmail.com>
2024-07-28 21:37:48 +0900

    [追加] バージョン不一致通知機能

--- src/main/kotlin/com/github/Ringoame196/Main.kt
+++ src/main/kotlin/com/github/Ringoame196/Main.kt
@@ -2,6 +2,7 @@ package com.github.Ringoame196
 
 import com.github.Ringoame196.command.Command
 import com.github.Ringoame196.command.TabComplete
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
@@ -10,5 +11,14 @@ class Main : JavaPlugin() {
         val command = getCommand("itemname")
         command!!.setExecutor(Command())
         command.tabCompleter = TabComplete()
+
+        notificationVersionMismatch()
+    }
+    private fun notificationVersionMismatch() {
+        val pluginVersion = description.apiVersion
+        val mcVersion = Bukkit.getVersion()
+        if (pluginVersion != mcVersion) {
+            Bukkit.getConsoleSender().sendMessage("[${this.name}] Some items may not be compatible due to version mismatch(version:$pluginVersion)")
+        }
     }
 }
